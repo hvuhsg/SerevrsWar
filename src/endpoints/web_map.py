744e9f05d75c247ee67317a6fa0d831ac48ec2c2
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-02-08 01:03:48 +0200

    Get token from url params

--- src/endpoints/web_map.py
+++ src/endpoints/web_map.py
@@ -8,8 +8,7 @@ router = APIRouter()
 
 
 @router.get("/guiMap")
-def gui_map(request: Request, token: str, db=Depends(get_db)):
-    print(vars(request))
+def gui_map(token: str, db=Depends(get_db)):
     player = db["players"].find_one({"token": token})
     if not player:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Token")
